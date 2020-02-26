8fe4da8a443af3b6dd480d69f8aeafea96cab235
carturo6 <arturo.carlos.nolasco@gmail.com>
2020-02-25 22:12:59 -0600

    Empezando con pantalla login

---
+++ app/src/main/java/com/example/app_registry/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.app_registry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app_registry.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/app_registry/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.app_registry.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
