b6d6a66baacb237cf793fc39ddfcece9543d5cd2
carturo6 <arturo.carlos.nolasco@gmail.com>
2020-02-27 18:18:05 -0600

    Agregando el Activity de lista

---
+++ app/src/main/java/com/example/app_registry/Activity_List.java
@@ -0,0 +1,14 @@
+package com.example.app_registry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
