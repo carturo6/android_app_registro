0735de2cd1d4e211fe3337b05df457ab014dd394
carturo6 <arturo.carlos.nolasco@gmail.com>
2020-02-27 21:59:41 -0600

    Añadiendo codigo para cambiar de la 1er activity al 2

--- app/src/main/java/com/example/app_registry/MainActivity.java
+++ app/src/main/java/com/example/app_registry/MainActivity.java
@@ -2,20 +2,51 @@ package com.example.app_registry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.app_registry.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View btn;
+    private final String GREET = "Wellcom";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
+
+        //Haciendo referencia al boton
+        btn = (Button)findViewById(R.id.btnRedondo);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Accion para el boton. Cuando se clickea sale un mensaje.
+                Toast.makeText(MainActivity.this, "Buton click", Toast.LENGTH_LONG).show();
+
+                //Accediando al segundo activity - pantall del activity_list
+                Intent intent = new Intent(MainActivity.this, Activity_List.class);
+                intent.putExtra("greeter", GREET);
+                startActivity(intent);
+
+
+            }
+        });
+
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
-        }
+        }*/
+    }
+
+
+    public void next(View v){
+
     }
 }
