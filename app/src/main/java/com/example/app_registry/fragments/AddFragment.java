061b92c2ad9b5a440dec30ab34b612eb36bc5348
carturo6 <arturo.carlos.nolasco@gmail.com>
2020-03-15 23:49:58 -0600

    Empezando a traer la fecha del sistema

--- app/src/main/java/com/example/app_registry/fragments/AddFragment.java
+++ app/src/main/java/com/example/app_registry/fragments/AddFragment.java
@@ -8,14 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.app_registry.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddFragment extends Fragment {
 
+    EditText hora;
 
     public AddFragment() {
         // Required empty public constructor
@@ -23,9 +28,16 @@ public class AddFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        Date date = new Date();
+        hora = (EditText) hora.findViewById(R.id.hora);
+        SimpleDateFormat horaC=new SimpleDateFormat("d, MMMM 'del', yyyy");
+        String sHora = horaC.format(date);
+        hora.setText(sHora);
+
+
         return inflater.inflate(R.layout.fragment_add, container, false);
     }
 
